ce26b639b9ac5458eba5cd9f4cd817fd9ab00a26
Tomilola <83653154+Bluel0la@users.noreply.github.com>
2025-08-15 16:32:43 +0100

    Add project routes to API v1 router

--- api/v1/routes/__init__.py
+++ api/v1/routes/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from api.v1.routes.authentication import auth
+from api.v1.routes.project import projects
 api_version_one = APIRouter(prefix="/api/v1")
 
 api_version_one.include_router(auth)
+api_version_one.include_router(projects)
